fb32fa34143cea46a4236b13eeecf353538c7399
romeoristimae <romeor@taltech.ee>
2026-03-05 16:44:55 +0200

    testide baasklass tehrtud

--- Tests/Tests.Aids/MovieTests.cs
+++ Tests/Tests.Aids/MovieTests.cs
@@ -2,9 +2,13 @@
 
 namespace Abc.Tests.Aids
 {
-    [TestClass] public sealed class MovieTests {
-        private Movie movie;
-        [TestInitialize] public void TestInitialize() => movie = new Movie();
-        [TestMethod] public void CanCreateTest() => Assert.IsNotNull(movie);
+    [TestClass] public sealed class MovieTests : BaseTests<Movie>{}
+    [TestClass] public sealed class CountryTests : BaseTests<Country> { }
+    [TestClass] public sealed class CurrencyTests : BaseTests<Currency> { }
+    public abstract class BaseTests <TClass> where TClass : class, new()
+    {
+        private TClass obj;
+        [TestInitialize] public void TestInitialize() => obj = new TClass();
+        [TestMethod] public void CanCreateTest() => Assert.IsNotNull(obj);
     }
 }
