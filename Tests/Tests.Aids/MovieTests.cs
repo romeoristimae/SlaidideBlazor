b748b0cb5e8d411738f3d09e9cb2ab4adfbcba1b
romeoristimae <romeor@taltech.ee>
2026-03-05 16:23:29 +0200

    Esimesed testid tehtud.

---
+++ Tests/Tests.Aids/MovieTests.cs
@@ -0,0 +1,10 @@
+﻿using Abc.Soft.Movie.Models;
+
+namespace Abc.Tests.Aids
+{
+    [TestClass] public sealed class MovieTests {
+        private Movie movie;
+        [TestInitialize] public void TestInitialize() => movie = new Movie();
+        [TestMethod] public void CanCreateTest() => Assert.IsNotNull(movie);
+    }
+}
