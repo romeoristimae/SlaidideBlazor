aea7339d98c5d744d0b47c8258c900b4d762bbf8
romeoristimae <romeor@taltech.ee>
2026-03-12 14:39:00 +0200

    testide refaktoreerimine tehtud

---
+++ Code/Aids/GetType.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace Abc.Aids
+{
+    public static class GetType{
+        public static IEnumerable<PropertyInfo> Properties<TClass>(BindingFlags f) 
+            => typeof(TClass).GetProperties(f);
+        public static IEnumerable<string> PropertyNames<TClass>(BindingFlags f) 
+            => Properties<TClass>(f).Select(p => p.Name);
+        public static IEnumerable<MethodInfo> Methods<TClass>(BindingFlags f, bool includeSpecialNames = false)
+            => Array.FindAll(typeof(TClass).GetMethods(f), i => includeSpecialNames || !i.IsSpecialName);
+        public static IEnumerable<string> MethodNames<TClass>(BindingFlags f, bool includeSpecialNames = false)
+            => Methods<TClass>(f, includeSpecialNames).Select(p => p.Name);
+
+        /*
+        private static IEnumerable<string> getProperties() => typeof(TClass)
+       .GetProperties(publicDeclared)
+       .Select(i => i.Name);
+
+        private static IEnumerable<string> getMethods() => Array.FindAll(
+            typeof(TClass).GetMethods(publicDeclared),
+            i => !i.IsSpecialName)
+            .Select(i => i.Name);
+        */
+    }
+}
--- Tests/Tests.Aids/BaseTests.cs
+++ Tests/Tests.Aids/BaseTests.cs
@@ -1,20 +1,34 @@
-﻿namespace Abc.Tests.Aids;
+﻿using Abc.Aids;
+using System.Linq;
+using System.Reflection;
+
+namespace Abc.Tests.Aids;
 
 public abstract class BaseTests <TClass> where TClass : class, new(){
      private TClass obj;
+    private const BindingFlags publicDeclared = BindingFlags.Public
+        | BindingFlags.DeclaredOnly
+        | BindingFlags.Instance
+        | BindingFlags.Static;
      [TestInitialize] public void TestInitialize() => obj = new TClass();
      [TestMethod] public void CanCreateTest() => Assert.IsNotNull(obj);
-     [TestMethod] public void IsClassTestedTest() {
+     [TestMethod] public void IsClassCorrectTest(){
         var className = typeof(TClass).Name;
         var testClassName = GetType().Name;
         Assert.AreEqual(testClassName.Replace("Tests", ""), className);
     }
-    [TestMethod]
-    public void IsClassCorrectTest()
-    {
-        var testMethods = GetType().GetMethods();
-        var classMembers = typeof(TClass).GetMembers();
-        Assert.Inconclusive("Not all members are tested");
+    [TestMethod] public void IsClassTestedTest(){
+        var testMethods = GetType().GetMethods().Select(x => x.Name);
+        var membersToTest = getProperties().Concat(getMethods());
+        foreach (var m in membersToTest) {
+           if ( !testMethods.Contains(m + "Test"))
+                Assert.Inconclusive($"{m} is not tested");
+        }
     }
+    private static IEnumerable<string> getProperties()
+        => global::Abc.Aids.GetType.PropertyNames<TClass>(publicDeclared);
+
+    private static IEnumerable<string> getMethods()
+        => global::Abc.Aids.GetType.MethodNames<TClass>(publicDeclared, false);
 }
 
